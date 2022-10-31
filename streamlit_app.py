f21c94b94767690dd1c981a200d92ab5a784a0dc
Enas Ali <90562632+enaasali@users.noreply.github.com>
2022-10-31 16:44:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 streamlit.title("My Mom's New Healthy Diner")
    
 streamlit.header('Breakfast Favorites')
@@ -9,7 +13,7 @@ streamlit.text('🥑🍞 Avogado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+# import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -24,7 +28,7 @@ streamlit.dataframe(fruits_to_show)
 #new section to show fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
+# import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 # input user fruit choice
@@ -37,8 +41,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 # Do not run anything until we troubleshoot
 streamlit.stop()
-import snowflake.connector
 
+# import snowflake.connector
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 # my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
