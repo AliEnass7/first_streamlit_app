9b128c7f8c5833d85e165b47063b62edf1ee6302
Enas Ali <90562632+enaasali@users.noreply.github.com>
2022-11-03 22:33:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-streamlit.title("My Mom's New Healthy Diner")
+streamlit.title("My Mom's New Healthy Dinner")
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -61,6 +61,7 @@ if streamlit.button("Get Fruit Load List"):
 def insert_raw_snowflake(new_fruit):
    with my_cnx.cursor() as  my_cur:
       my_cur.execute("insert into fruit_load_list value ('from streamlit')")
+      my_cur.execute("delete from fruit_load_list where fruit_name= 'streamlit'")
       return "Thank you for adding a new fruit"+ new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
